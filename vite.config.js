c5c705da571f3e3e5478825c3ba5d9cd99112cbf
Ihor Rusenko <rusenko.i@outlook.com>
2022-02-19 15:34:07 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,36 @@ import path from 'path'
 import {
   defineConfig
 } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 
 const PATHS = {
+  src: path.resolve(__dirname, './src'),
   output: path.resolve(__dirname, './public_html'),
+  static: path.resolve(__dirname, './src/static'),
+  css: path.resolve(__dirname, './src/scss')
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: PATHS.static, // where is index.html
+
   server: {
     port: 80
   },
 
   plugins: [
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    }),
     vue()
   ],
 
   build: {
     emptyOutDir: true,
-    outDir: PATHS.output
-  }
+    outDir: PATHS.output,
+
+    rollupOptions: {
+      input: {
+        main: `${PATHS.static}/index.html`,
+      }
+    }
+  },
 
 })
